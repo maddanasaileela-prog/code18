45c5ff9ef2774e1b45c867fa73aa3fb3075f7d11
maddanasaileela-prog <maddanasaileela@gmail.com>
2025-12-09 23:08:04 +0530

    Add files via upload

---
+++ add two numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,sum=0;
+    // Write C code here
+    printf("Enter any two integer Numbers");
+    scanf("%d,%d",&a,&b);
+    sum=a+b;
+   printf("The sum of %d and %d is %d",a,b,sum);
+
+    return 0;
+}
